e319545e90e6bf399b7008e104590bdbcfe5b01e
Austin K <austink@Austins-MacBook-Pro.local>
2024-07-16 18:34:14 -0400

    Passing US3 added test final commit

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,8 @@ class Recipe < ApplicationRecord
    validates :name, :complexity, :genre, presence: true
    has_many :recipe_ingredients
    has_many :ingredients, through: :recipe_ingredients
+
+   def total_cost
+      ingredients.sum(:cost)
+   end
 end
