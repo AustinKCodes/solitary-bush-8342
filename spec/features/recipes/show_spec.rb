17bbdb0caabe8e0807665bbe0be571f28f4d98ad
Austin K <austink@Austins-MacBook-Pro.local>
2024-07-16 18:30:33 -0400

    Adding test for US3

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -4,9 +4,11 @@ RSpec.describe "Recipe Show" do
   it "shows the recipe's name, complexity, genre, and ingredients" do
     recipe = Recipe.create!(name: "Spaghetti", complexity: 2, genre: "Italian")
     ingredient1 = Ingredient.create!(name: "Pasta", cost: 3)
-    ingredient2 = Ingredient.create!(name: "Tomato Sauce", cost: 1)
+    ingredient2 = Ingredient.create!(name: "Tomato Sauce", cost: 2)
+    ingredient3 = Ingredient.create!(name: "Ground Beef", cost: 5)
     RecipeIngredient.create!(recipe: recipe, ingredient: ingredient1)
     RecipeIngredient.create!(recipe: recipe, ingredient: ingredient2)
+    RecipeIngredient.create!(recipe: recipe, ingredient: ingredient3)
 
     visit "/recipes/#{recipe.id}"
 
@@ -15,5 +17,7 @@ RSpec.describe "Recipe Show" do
     expect(page).to have_content("Genre: Italian")
     expect(page).to have_content("Pasta")
     expect(page).to have_content("Tomato Sauce")
+    expect(page).to have_content("Ground Beef")
+    expect(page).to have_content("Total Cost: 10")
   end
 end
